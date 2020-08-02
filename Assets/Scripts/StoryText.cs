747974afad8cf3fa9a419345f39a5d03726576ea
Bruce Moe <brucemoe1@gmail.com>
2020-08-01 20:48:37 -0500

    Initial Build
    Version 1.0 of League of Choices

---
+++ Assets/Scripts/StoryText.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class StoryText : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI textComponent;
+    [SerializeField] State startingState;
+    
+    State state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.getStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        manageState();
+    }
+
+    private void manageState() {
+        var nextStates = state.getNextStates();
+        for(int i = 0; i < nextStates.Length; i++)
+        {
+            if(Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                state = nextStates[i];
+            }
+        }
+        textComponent.text = state.getStateStory();
+    }
+}
